e3846eb206f8fb1292c94b11986c30231b17463d
colbr <colbr243@gmail.com>
2024-08-27 14:46:34 +0200

    feat: add GA and GTM Google Analytics (GA4) Google Tag Manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource/montserrat";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import type { AppProps } from "next/app";
 
 import { AppContextProvider } from "../contexts";
@@ -11,12 +12,17 @@ import theme from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <AppContextProvider>
-        <MainView>
-          <Component {...pageProps} />
-        </MainView>
-      </AppContextProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <AppContextProvider>
+          <MainView>
+            <Component {...pageProps} />
+          </MainView>
+        </AppContextProvider>
+      </ChakraProvider>
+      {/* Analytics */}
+      <GoogleTagManager gtmId="GTM-TCR2V5HB" />
+      <GoogleAnalytics gaId="G-QT2SMJ9P6V" />
+    </>
   );
 }
